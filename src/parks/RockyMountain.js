d621f8df9bc3beca789ac6d3a1473464d897b79d
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-04-15 11:16:04 -0400

    commit tree 8a871c771ac46afdc01d25672c7dcc7d41300e2b

--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,11 +1,10 @@
-const trees = "Aspen and Pine";
+export const trees = "Aspen and Pine";
 
 function wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
 }
 
-function elevation() {
+export function elevation() {
   console.log("9583 ft");
 }
 
-export { trees, wildlife };
